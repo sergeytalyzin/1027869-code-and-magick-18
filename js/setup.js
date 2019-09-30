1f7ec14e1aa9f5231fca64a12f021dedbc43583c
Sergey <st@ttsgroup.su>
2019-09-30 14:54:51 +0300

    исправил

--- js/setup.js
+++ js/setup.js
@@ -12,8 +12,8 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
 
-var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
@@ -40,7 +40,7 @@ var generateWizards = function (length) {
   var array = [];
   for (var i = 0; i < length; i++) {
     var wizard = {
-      name: generateName(names, surnames),
+      name: generateName(NAMES, SURNAMES),
       coatColor: generateColor(coatColors),
       eyesColor: generateEyesColor(eyesColors)
     };
